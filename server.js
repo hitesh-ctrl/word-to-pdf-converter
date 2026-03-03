e2a9add1db36a698e7b24abc3578b90fdb0cf5fa
Hitesh Venugopalan <hiteshvkrish@gmail.com>
2026-03-04 01:16:38 +0530

    changed chromium path

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ let browser;
   
 browser = await puppeteer.launch({
   headless: true,
-  executablePath: '/usr/bin/chromium',
+  executablePath: '/snap/bin/chromium',
   args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu']
 });
   console.log("Puppeteer browser launched");
